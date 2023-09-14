0e74cda7a2f8e191258459b0d41acb4f2fe47382
Cooper Gregory <cooper.c.gregory@gmail.com>
2023-09-14 15:34:57 +1000

    Working on Badguy

---
+++ GameJamTime/Assets/Scripts/BadGuyRaycast.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BadGuyRaycast : MonoBehaviour
+{
+
+    public GameObject lastHit;
+    public Vector3 collision = Vector3.zero;
+
+    [Header("Layers")]
+    public LayerMask playerHit;
+    public LayerMask wallHit;
+    public float raycastRange = 100;
+
+
+    public NavMeshAgent agent;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+
+        var ray = new Ray(this.transform.position, this.transform.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, raycastRange, playerHit))
+        {
+            agent.transform.position = hit.point;
+            collision = hit.point;
+        }
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(collision, radius: 0.2f);
+    }
+}
